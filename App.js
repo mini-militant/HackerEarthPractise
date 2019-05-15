8c18ad0c6d978caf613af7e4d7d228ad78f58242
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-15 09:21:20 +0530

    2 files modified

--- App.js
+++ App.js
@@ -4,16 +4,10 @@ import ISP from './components/ISP'
 import ISP_data from './components/ISP_data'
 
 function App(){
-
+{/* all the data is mapped via object named "data" */}
  const ISP_data_mapping = ISP_data.map(data => 
-  <ISP key={data.id} data={data}/>
+  <ISP key={data.id} ISPdata={data}/>  
   );
- 
-
- 
-
- 
-
  
     return (
       <div>
--- components/ISP.js
+++ components/ISP.js
@@ -3,14 +3,14 @@ import React from "react"
 const ISP = (props) =>{
   return(
     <div>
-       <h3 > {props.data.name}</h3>
-        <h3> {props.data.lowest_price}</h3>
-        <h3> {props.data.rating}</h3>
-        <h3> {props.data.max_speed}</h3>
-        <h3> {props.data.description}</h3>
-        <h3> {props.data.contact_no}</h3>
-        <img src={props.data.image} alt="error"/>
-        <h3> {props.data.url}</h3>
+       <h3 > {props.ISPdata.name}</h3>
+        <h3> {props.ISPdata.lowest_price}</h3>
+        <h3> {props.ISPdata.rating}</h3>
+        <h3> {props.ISPdata.max_speed}</h3>
+        <h3> {props.ISPdata.description}</h3>
+        <h3> {props.ISPdata.contact_no}</h3>
+        <img src={props.ISPdata.image} alt="error"/>
+        <h3> {props.ISPdata.url}</h3>
     </div>
 
   );
