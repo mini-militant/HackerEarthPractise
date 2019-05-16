8d771882ea2d712e4337b5188260760423a7de39
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-16 17:50:33 +0530

    1 file modified

--- components/ISP.js
+++ components/ISP.js
@@ -11,24 +11,24 @@ const ISP = (props) =>{
         </div>
         </div>
         <div className="col">
+        <div className="col">
+                <img src={props.ISPdata.image} alt="error"/>
+          </div> 
+          <div className="col">
           <div className="card">
             <div className="card-body">
                 <p> <i class="material-icons">attach_money</i>&nbsp;&nbsp;
                      Min. Plan : {props.ISPdata.lowest_price} /- 
                 </p>
-
-                
-               
-                
-                <p>
-                <i class="material-icons">network_wifi</i> 
+                <p><i class="material-icons">network_wifi</i> 
                   &nbsp;&nbsp;
                    Max Speed :{props.ISPdata.max_speed}</p>
-                <h3> {props.ISPdata.description}</h3>
+                
                  <p><i class="material-icons">local_phone</i> 
                   &nbsp;&nbsp;
                    {props.ISPdata.contact_no}</p>
-                <img src={props.ISPdata.image} alt="error"/>
+                  
+                
                 <p><i class="material-icons">email</i>
                 &nbsp;&nbsp;
                 {props.ISPdata.email}
@@ -39,8 +39,16 @@ const ISP = (props) =>{
                    starDimension="35px"
                    starSpacing="0px"
                  />
+                 <div class="md-form">
+                 <i class="fas fa-pencil-alt prefix"></i>
+                  <textarea id="form10" class="md-textarea form-control" rows="3">
+                  {props.ISPdata.description}
+                  </textarea>
+                    <label for="form10">Icon Prefix</label>
+                </div>
                 <a href= {props.ISPdata.url}>Go to Website</a></div>
             </div>
+          </div>
           </div>
       </div>  
     </div>
