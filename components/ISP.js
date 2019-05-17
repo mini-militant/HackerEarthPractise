178de7585c1ee53c261b39413c69e0c5312a66c4
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-17 08:58:04 +0530

    2 files modified

--- components/ISP.js
+++ components/ISP.js
@@ -1,65 +1,69 @@
 import React from "react"
 import StarRating from 'react-star-ratings'
+import ShowData from './ShowData.js'
 
-function showData(){
 
-}
 
-const ISP = (props) =>{
+class ISP extends React.Component {
+
+
+render(){
   return(
     <div>
       <div className="row">
         <div className="col-6">
-          <button onClick="showData()" type="button" class="btn btn-outline-secondary btn-block">
-          {props.ISPdata.name}
+          <button  type="button" className="btn btn-outline-secondary btn-block">
+          {this.props.ISPdata.name}
           </button>
-        </div>
-
-        <div className="col">
+        </div><div className="col">
           <div className="row">
             <div className="col">
-                <img src={props.ISPdata.image} alt="error"/>
+                <img src={this.props.ISPdata.image} alt="error"/>
             </div> 
             <div className="col">
             <div className="card">
             <div className="card-body">
-                <p> <i class="material-icons">attach_money</i>&nbsp;&nbsp;
-                     Min. Plan : {props.ISPdata.lowest_price} /- 
+                <p> <i className="material-icons">attach_money</i>&nbsp;&nbsp;
+                     Min. Plan : {this.props.ISPdata.lowest_price} /- 
                 </p>
-                <p><i class="material-icons">network_wifi</i> 
+                <p><i className="material-icons">network_wifi</i> 
                   &nbsp;&nbsp;
-                   Max Speed :{props.ISPdata.max_speed}</p>
+                   Max Speed :{this.props.ISPdata.max_speed}</p>
                 
-                 <p><i class="material-icons">local_phone</i> 
+                 <p><i className="material-icons">local_phone</i> 
                   &nbsp;&nbsp;
-                   {props.ISPdata.contact_no}</p>
-                <p><i class="material-icons">email</i>
+                   {this.props.ISPdata.contact_no}</p>
+                <p><i className="material-icons">email</i>
                 &nbsp;&nbsp;
-                {props.ISPdata.email}
+                {this.props.ISPdata.email}
                 </p>
 
                 <StarRating
                    starRatedColor="#8c9906"
-                   rating={props.ISPdata.rating}
+                   rating={this.props.ISPdata.rating}
                    starDimension="35px"
                    starSpacing="0px"
                  />
-                 <div class="md-form">
-                    <i class="fas fa-pencil-alt prefix"></i>
-                    <textarea id="form10" class="md-textarea form-control" rows="3" value={props.ISPdata.description}>
+                 <div className="md-form">
+                    <i className="fas fa-pencil-alt prefix"></i>
+                    <textarea id="form10" className="md-textarea form-control" rows="3" value={this.props.ISPdata.description}>
                     
                     </textarea>
                   </div>
-                <a href= {props.ISPdata.url}>Go to Website</a></div>
+                <a href= {this.props.ISPdata.url}>Go to Website</a></div>
             </div>
           </div>
-          
-          </div>
-          </div>
-      </div>  
+          </div>  
+      <ShowData/>
     </div>
 
-  );
+        
+          </div>
+          </div>
+      
+
+  )
+}
 }
 
 
--- components/ShowData.js
+++ components/ShowData.js
@@ -4,47 +4,7 @@ class ShowData extends React.Component{
   render(){
     return(
       <div>
-        <div className="col-6">
-          <div className="row">
-            <div className="col">
-                <img src={props.ISPdata.image} alt="error"/>
-            </div> 
-            <div className="col">
-            <div className="card">
-            <div className="card-body">
-                <p> <i class="material-icons">attach_money</i>&nbsp;&nbsp;
-                     Min. Plan : {props.ISPdata.lowest_price} /- 
-                </p>
-                <p><i class="material-icons">network_wifi</i> 
-                  &nbsp;&nbsp;
-                   Max Speed :{props.ISPdata.max_speed}</p>
-                
-                 <p><i class="material-icons">local_phone</i> 
-                  &nbsp;&nbsp;
-                   {props.ISPdata.contact_no}</p>
-                <p><i class="material-icons">email</i>
-                &nbsp;&nbsp;
-                {props.ISPdata.email}
-                </p>
-
-                <StarRating
-                   starRatedColor="#8c9906"
-                   rating={props.ISPdata.rating}
-                   starDimension="35px"
-                   starSpacing="0px"
-                 />
-                 <div class="md-form">
-                    <i class="fas fa-pencil-alt prefix"></i>
-                    <textarea id="form10" class="md-textarea form-control" rows="3" value={props.ISPdata.description}>
-                    
-                    </textarea>
-                  </div>
-                <a href= {props.ISPdata.url}>Go to Website</a></div>
-            </div>
-          </div>
-          
-          </div>
-          </div>
+        anything
       </div>
 
     );
